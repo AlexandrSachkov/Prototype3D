b6b2ddf47383c518dbf86d12aa13a401874b1ede
Alex Sachkov <alexsachkov@gmail.com>
2018-11-10 22:46:41 -0500

    refactored triangle sample and moved it to samples directory

--- src/samples/Sample2_Triangle.cpp
+++ src/samples/Sample2_Triangle.cpp
@@ -1,23 +1,22 @@
-#include "util/util_DefaultSampleRunner.h"
+#include "../util/util_DefaultSampleRunner.h"
 
-#include "assert.h"
-#include "device/RenderingDeviceI.h"
-#include "device/Texture1dArrayI.h"
-#include "device/Texture2dArrayI.h"
-#include "device/Texture3dI.h"
-#include "device/VertexShaderI.h"
-#include "device/PixelShaderI.h"
-#include "device/BufferI.h"
-#include "device/RasterizerI.h"
-#include "Constants.h"
+#include "../assert.h"
+#include "../device/RenderingDeviceI.h"
+#include "../device/Texture1dArrayI.h"
+#include "../device/Texture2dArrayI.h"
+#include "../device/Texture3dI.h"
+#include "../device/VertexShaderI.h"
+#include "../device/PixelShaderI.h"
+#include "../device/BufferI.h"
+#include "../device/RasterizerI.h"
+#include "../Constants.h"
 
 #include "glm/vec3.hpp"
 
 #include <memory>
-#include <assert.h>
+#include <string>
 
 bool run() {
-    // Window initialization
     const bool fullscreen = false;
     const unsigned int windowDim[] = {800, 600};
     const std::string winTitle = "Prototype3D sample 2: Triangle";
@@ -27,39 +26,30 @@ bool run() {
         "Failed to initialize window manager");
 
 
-
-    p3d::RenderingDeviceI* device = nullptr;
+    std::unique_ptr<p3d::RenderingDeviceI> device = nullptr;
 #ifdef P3D_API_D3D11 
     p3d::d3d11::RenderingDevice* d3d11_device = new p3d::d3d11::RenderingDevice();
     P3D_ASSERT_R(d3d11_device->initialize(
         sampleRunner.getWindowHandle(),
         windowDim,
-        60,
-        4,
-        1,
-        false
+        60, // refresh rate
+        1,  // multi-sampling
+        1,  // number of back buffers (1 for double buffering, 2 for tripple)
+        fullscreen
     ), "Failed to initialize D3D11 device");
-    device = d3d11_device;
+    device.reset(d3d11_device);
 #endif
 
-    std::unique_ptr <p3d::Texture1dArrayI> texArr1d = nullptr;
-    P3D_ASSERT_R(device->createTexture1dArray({}, texArr1d), "Failed to create 1d texture array");
-
-    std::unique_ptr <p3d::Texture2dArrayI> texArr2d = nullptr;
-    P3D_ASSERT_R(device->createTexture2dArray({}, texArr2d), "Failed to create 2d texture array");
-
-    std::unique_ptr <p3d::Texture3dI> tex3d = nullptr;
-    P3D_ASSERT_R(device->createTexture3d({}, tex3d), "Failed to create 3d texture");
-
+    const std::string POSITION_IE = "POSITION";
     p3d::VertexShaderDesc vsDesc;
     vsDesc.shaderEntryPoint = "main";
     vsDesc.hlslSource =
-        "float4 main( float3 pos : POSITION ) : SV_POSITION { "
+        "float4 "+ vsDesc.shaderEntryPoint +"( float3 pos : " + POSITION_IE + " ) : SV_POSITION { "
         "	return float4(pos, 1.0f); "  
         "}"
         ;
     vsDesc.inputDesc = {
-        {"POSITION", p3d::P3D_FORMAT::P3D_FORMAT_R32G32B32_FLOAT, 12, 0}
+        { POSITION_IE, p3d::P3D_FORMAT::P3D_FORMAT_R32G32B32_FLOAT, 12, 0 }
     };
     std::unique_ptr <p3d::VertexShaderI> vs = nullptr;
     P3D_ASSERT_R(device->createVertexShader(vsDesc, vs), "Failed to create vertex shader");
@@ -76,22 +66,13 @@ bool run() {
     P3D_ASSERT_R(device->createPixelShader(psDesc, ps), "Failed to create pixel shader");
     device->PSSetShader(ps.get());
 
-    p3d::BufferDesc buffDesc;
-    buffDesc.bindFlags = { p3d::P3D_BIND_SHADER_RESOURCE };
-    buffDesc.usageFlag = p3d::P3D_USAGE_CPU_UPDATE_GPU_RW;
-    buffDesc.data = nullptr;
-    buffDesc.length = 1;
-    buffDesc.strideBytes = 200;
-    std::unique_ptr <p3d::BufferI> buffer = nullptr;
-    P3D_ASSERT_R(device->createBuffer(buffDesc, buffer), "Failed to create buffer");
-
-    p3d::BufferDesc buffDesc2;
-    buffDesc2.bindFlags = { p3d::P3D_BIND_VERTEX_BUFFER };
-    buffDesc2.usageFlag = p3d::P3D_USAGE_CPU_UPDATE_GPU_RW;
-    std::unique_ptr <p3d::BufferI> buffer2 = nullptr;
-    std::vector<glm::vec3> bufferData2 = { {0,0.5f,0}, {-0.5f,0,0}, {0,0,0.5f} };
-    P3D_ASSERT_R(device->createBuffer(buffDesc2, bufferData2, buffer2), "Failed to create buffer2");
-    device->IASetVertexBuffer(buffer2.get(), 0, 0);
+    p3d::BufferDesc vertexBuffDesc;
+    vertexBuffDesc.bindFlags = { p3d::P3D_BIND_VERTEX_BUFFER };
+    vertexBuffDesc.usageFlag = p3d::P3D_USAGE_CPU_UPDATE_GPU_RW;
+    std::unique_ptr <p3d::BufferI> vertexBuff = nullptr;
+    std::vector<glm::vec3> vertices = { {0,0.5f,0}, {-0.5f,0,0}, {0,0,0.5f} };
+    P3D_ASSERT_R(device->createBuffer(vertexBuffDesc, vertices, vertexBuff), "Failed to create buffer2");
+    device->IASetVertexBuffer(vertexBuff.get(), 0, 0);
     device->IASetPrimitiveTopology(p3d::P3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
     p3d::RasterizerDesc rastDesc;
@@ -102,9 +83,9 @@ bool run() {
     P3D_ASSERT_R(device->createRasterizer(rastDesc, rast), "Failed to create rasterizer");
     device->RSSetState(rast.get());
 
-    float topLeft[] = { 0.0f,0.0f };
-    float dimensions[] = { (float)windowDim[0], (float)windowDim[1] };
-    float minMaxDepth[] = { 0.0f, 1.0f };
+    const float topLeft[] = { 0.0f,0.0f };
+    const float dimensions[] = { (float)windowDim[0], (float)windowDim[1] };
+    const float minMaxDepth[] = { 0.0f, 1.0f };
     device->RSSetViewport(topLeft, dimensions, minMaxDepth);
 
     p3d::Texture2dArrayI& renderTargetBuff = device->getRenderTargetBuff();
@@ -112,11 +93,11 @@ bool run() {
     device->OMSetRenderTargets(&renderTargetBuff, &depthStencilBuff);
 
     sampleRunner.setRunProcedure([&]() {
-        float color[] = { 0.0f, 1.0f, 0.0f, 1.0f };
+        const float color[] = { 0.0f, 1.0f, 0.0f, 1.0f };
         device->clearRenderTargetBuff(&renderTargetBuff, color);
         device->clearDepthStencilBuff(&depthStencilBuff, 1.0f, 0);
 
-        device->draw(buffer2->getDescription().length, 0);
+        device->draw(vertexBuff->getDescription().length, 0);
         device->presentFrame();
     });
     sampleRunner.start();
