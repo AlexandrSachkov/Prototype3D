c0ec21a9288745a6fc5c95877968ff5dd50af34b
Alex Sachkov <alexsachkov@gmail.com>
2019-09-17 22:26:27 -0400

    added lighting example

---
+++ src/samples/Sample7/Lighting.cpp
@@ -0,0 +1,82 @@
+#include "p3d/util/util_DefaultSampleRunner.h"
+
+#include "p3d/assert.h"
+#include "p3d/renderer/d3d11/d3d11_Renderer.h"
+#include "p3d/renderer/RendererI.h"
+#include "p3d/renderer/Texture1dArrayI.h"
+#include "p3d/renderer/Texture2dArrayI.h"
+#include "p3d/renderer/Texture3dI.h"
+#include "p3d/renderer/VertexShaderI.h"
+#include "p3d/renderer/PixelShaderI.h"
+#include "p3d/Constants.h"
+#include "p3d/scene/Scene.h"
+#include "p3d/scene/NullSpacePartitioner.h"
+#include "p3d/util/util_SceneImporter.h"
+#include "p3d/scene/PerspectiveCamera.h"
+
+#include "glm/vec3.hpp"
+
+#include <memory>
+#include <string>
+
+bool run() {
+    const bool fullscreen = false;
+    const unsigned int windowDim[] = { 1920, 1080 };
+    const std::string winTitle = "Prototype3D sample 1: Create window";
+
+    auto sampleRunner = p3d::util::DefaultSampleRunner();
+    P3D_ASSERT_R(sampleRunner.initialize(winTitle, windowDim, fullscreen, true, false, false),
+        "Failed to initialize window manager");
+
+    unsigned int clientDim[2];
+    sampleRunner.getClientSize(clientDim);
+
+    std::unique_ptr <p3d::d3d11::Renderer> d3d11Renderer(new p3d::d3d11::Renderer());
+    P3D_ASSERT_R(d3d11Renderer->initialize(
+        sampleRunner.getWindowHandle(),
+        clientDim,
+        60, // refresh rate
+        1,  // multi-sampling
+        16,  // anisotropy
+        1,  // number of back buffers (1 for double buffering, 2 for tripple)
+        fullscreen
+    ), "Failed to initialize D3D11 device");
+
+    std::unique_ptr<p3d::RendererI> renderer(d3d11Renderer.release());
+    std::unique_ptr<p3d::SceneI> sponzaScene(new p3d::Scene(
+        std::unique_ptr<p3d::SpacePartitionerI>(new p3d::NullSpacePartitioner()), 
+        renderer.get()
+    ));
+
+    P3D_ASSERT_R(
+        p3d::util::SceneImporter::import("D:/Repositories/Prototype3D/resources/sponza/sponza.obj", "", sponzaScene.get(), nullptr),
+        "Failed to import cube.obj");
+
+    p3d::SceneProperties sceneProperties = sponzaScene->getProperties();
+    sceneProperties.ambientLight = { 0.9f,0.9f,0.9f };
+    sceneProperties.drawBoundingVolumes = false;
+    sponzaScene->setProperties(sceneProperties);
+
+    //Field of view should be 45 degrees. Otherwise, it will cause artifacts (need to look into perspective distortion)
+    //Keep near/far planes closer together to avoid floating precision issues
+    std::unique_ptr<p3d::PerspectiveCamera> camera(
+        new p3d::PerspectiveCamera({ 0,0,-2 }, 45.0f, (float)clientDim[0] / clientDim[1], 1.0f, 10000.0f)
+    );
+
+    sampleRunner.setRunProcedure([&]() {
+        renderer->renderFrame(sponzaScene.get(), camera.get());
+    });
+    sampleRunner.setCameraController(camera.get(), 0.2f, 0.001f);
+    sampleRunner.enableDebugging(sponzaScene.get());
+    sampleRunner.start();
+
+    return true;
+}
+
+int main() {
+    if (run()) {
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_FAILURE;
+}
